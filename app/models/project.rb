918770f5cd08bad5b8382af2d7076c6f95cfc48d
Enzo Vergara <vergara.enzo@gmail.com>
2013-05-28 23:12:30 +0800

    trying add tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name, :team_id
   belongs_to :team
+  has_many :tasks
   validates :name, :presence => true, :uniqueness => {scope: :team_id}
 end
